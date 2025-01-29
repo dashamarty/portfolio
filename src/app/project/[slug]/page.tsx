ffdd80a6f03fe936b524696265f9091b309d8af8
dashamarty <sinkodaria6@gmail.com>
2025-01-29 14:18:18 +0100

    fix build

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -2,7 +2,12 @@ import { folders } from "@/data/projects";
 import styles from "./page.module.scss";
 import Image from "next/image";
 
-export default function ProjectPage({ params }: { params: { slug: string } }) {
+// Типизация параметров
+interface ProjectPageProps {
+  params: { slug: string };
+}
+
+export default function ProjectPage({ params }: ProjectPageProps) {
   const project = folders
     .flatMap((folder) => folder.projects)
     .find((p) => p.id === params.slug);
@@ -15,7 +20,12 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
       <p>{project.description}</p>
       {project.details.map((detail, index) => (
         <div key={index} className={styles.projectDetail}>
-          <Image src={detail.image} alt={detail.text} />
+          <Image
+            src={detail.image}
+            alt={detail.text}
+            width={500}
+            height={300}
+          />
           <p>{detail.text}</p>
         </div>
       ))}
